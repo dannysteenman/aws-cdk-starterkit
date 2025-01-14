93753cb51b1d6885c2b7d43ed30ec12a59d3a77f
Danny Steenman <danny@towardsthecloud.com>
2025-01-14 15:11:43 +0100

    refactor(stack): Rename BaseStack to StarterStack in test file

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,10 +1,10 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { BaseStack } from '../src/stacks';
+import { StarterStack } from '../src/stacks';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new BaseStack(app, 'test', {});
+  const stack = new StarterStack(app, 'test', {});
 
   const template = Template.fromStack(stack);
   expect(template.toJSON()).toMatchSnapshot();
