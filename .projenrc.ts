5eb9dfb54b8205a190999158b412e628f1d9a91b
Danny Steenman <danny@towardsthecloud.com>
2025-01-31 13:53:56 +0100

    build(deps): bump projen and aws-cdk-lib to latest versions

--- .projenrc.ts
+++ .projenrc.ts
@@ -29,12 +29,12 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   authorOrganization: true,
   name: 'aws-cdk-starterkit',
   description: 'Create and deploy an AWS CDK app on your AWS account in less than 5 minutes using GitHub actions!',
-  cdkVersion: '2.176.0', // Find the latest CDK version here: https://www.npmjs.com/package/aws-cdk-lib
+  cdkVersion: '2.177.0', // Find the latest CDK version here: https://www.npmjs.com/package/aws-cdk-lib
   cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   packageManager: NodePackageManager.NPM,
   minNodeVersion: nodeVersion,
-  projenVersion: '0.91.6', // Find the latest projen version here: https://www.npmjs.com/package/projen
+  projenVersion: '0.91.7', // Find the latest projen version here: https://www.npmjs.com/package/projen
   projenrcTs: true,
   release: true,
   deps: ['cloudstructs'] /* Runtime dependencies of this module. */,
